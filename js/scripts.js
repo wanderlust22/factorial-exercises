e8a843244cb6cf082b985f25089e457c6166437a
Trevor Mackin and Will Quanstrom <will@>
2019-10-29 16:43:00 -0700

    code that takes a number input and returns its factorial

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,40 @@
 $(document).ready(function() {
 
+  var total = 1;
 
-
+  function factorial2(number){
+    for (let i = 1; i <= number; i++){
+      total *= i;
+    }
+  }
+  factorial2(5);
+  console.log(total);
 });
+
+
+
+//
+// $(document).ready(function() {
+//
+//   let numberArray = [];
+//
+//   function factorial(number) {
+//     for (var i = 1; i <= number; i++){
+//       numberArray.push(i);
+//     }
+//     multiply(numberArray);
+//   }
+//
+//   factorial(5);
+//
+//   console.log(numberArray);
+//
+//   function multiply(array){
+//     var total = 1;
+//     for (let i = 0; i < array.length; i++){
+//       total *= array[i];
+//     }
+//     console.log(total);
+//   }
+//
+// });
